8f4dd36009a5bb6bc4639bb80b4e270befe31a26
Sayed161 <sheikhsayed23@gmail.com>
2024-07-04 21:26:17 +0600

    Statics files

--- DevCluster/settings.py
+++ DevCluster/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+kbnx7ct_z6+tr#fm*ocpmc6+omz0u_+qe)pdgahuw4(r_e61('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app']
+LLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
 
 import dj_database_url
@@ -91,13 +91,17 @@ AUTH_USER_MODEL = 'Users.User'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'UFMyLSxpslMVKyygtzWVjCThCeXheQIN',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '51006',
     }
 }
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -140,6 +144,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
